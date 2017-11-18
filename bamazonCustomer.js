f97f0bf2c871cbb276d9a95bb9e1ce8316948817
Trevor Meadows <tlm04070@gmail.com>
2017-11-17 21:26:09 -0500

    done with first part

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,6 +13,7 @@ connection.connect(function (err) {
     runSearch();
 });
 
+let runningTotal = 0;
 
 function runSearch() {
     inquirer
@@ -22,29 +23,81 @@ function runSearch() {
             message: "Whats the ID?",
 
         }).then(function (answer) {
-            console.log(answer);
             var query = 'SELECT product_name FROM products WHERE ?';
             connection.query(query, {
                 item_id: answer.item_id,
             }, function (err, res) {
+                var answer1 = res[0].product_name;
                 console.log("Item: " + res[0].product_name);
-                quantity();
+                itemQuan(answer1);
 
             });
 
         })
 };
 
-function quantity() {
+function itemQuan(answer1) {
     inquirer
         .prompt({
             name: "quantity",
             type: "input",
             message: "Sure thing, how many would you like?"
-        }).then(function (answer) {
-            var query = "SELECT stock_quantity FROM products WHERE ?";
+        }).then(function (ordered) {
+            var query = "SELECT stock_quantity, price FROM products WHERE ?";
             connection.query(query, {
+                product_name: answer1
+            }, function (err, res) {
+                var singlePrice = res[0].price;
+                var stock = res[0].stock_quantity;
+                console.log("ordered quantity: " + ordered.quantity);
+                console.log(answer1 + " stock: " + stock)
+                var difference = parseFloat(stock) - parseFloat(ordered.quantity);
+                var currentTotal = parseFloat(singlePrice) * parseFloat(ordered.quantity);
+                runningTotal += currentTotal;
+                console.log("running total: " + runningTotal);
+                if (ordered.quantity > stock) {
+                    console.log("Insufficient quantity!");
+                    runSearch();
+                } else {
+                    updateTable(difference, answer1);
 
-            })
+                }
+            });
+
         })
+}
+
+function updateTable(difference, answer1) {
+    var query = "UPDATE products SET ? WHERE ?";
+    connection.query(query, [{
+            stock_quantity: difference
+        },
+        {
+            product_name: answer1
+        }
+    ], function (err, res) {
+        console.log("remaining " + answer1 + " stock: " + difference);
+        anotherOrder();
+    });
+
+}
+
+function anotherOrder() {
+    inquirer
+        .prompt({
+            name: "runAgain",
+            type: "list",
+            message: "Anything else?",
+            choices: [
+                "Yes",
+                "No"
+            ]
+        }).then(function (choice) {
+            if (choice.runAgain === 'Yes') {
+                console.log("picked yes");
+                runSearch();
+            } else {
+                return console.log("That brings your total to $" + runningTotal + ", come again!")
+            }
+        });
 }
